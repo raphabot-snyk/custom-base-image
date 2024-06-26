e1c58cf6462e66a9a0bc626ad21153fe875e8b8d
Raphael Bottino <raphael.bottino@snyk.io>
2024-06-26 08:39:39 -0500

    Fixing a dumb mistake

--- node/app/index.js
+++ node/app/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var _ = require('lodash');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-  res.send(`Current timestamp is: ${_.now()}`);
+  res.send(`Hello World! Current timestamp is: ${_.now()}.`);
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
